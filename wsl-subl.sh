eb313ddb8942383cb0c2efb804f02eb492b38198
Al Johri <al.johri@gmail.com>
2021-01-03 04:30:54 -0500

    enable `subl --wait` for git EDITOR

--- wsl-subl.sh
+++ wsl-subl.sh
@@ -2,8 +2,13 @@
 
 set -e
 
+if [ $1 = "--wait" ]; then
+  args="--wait"
+  shift 1
+fi
+
 if [ $# -gt 0 ]; then
     path=$(wslpath -aw $*)    
 fi
 
-"/mnt/c/Program Files/Sublime Text 3/subl.exe" $path
+"/mnt/c/Program Files/Sublime Text 3/subl.exe" $args $path
